7788a36c64ecc0631d153c76718eaa5f0a7d9517
Maria Vitória Jerônimo <mariavitoriajeronimo009@gmail.com>
2025-11-07 18:25:32 -0300

    Add files via upload

---
+++ Portifólio - Maria Vitoria/script.js
@@ -0,0 +1,8 @@
+/*function mudarTela(IdnovaTela) {
+const telaAtiva = document.querySelector('.tela.ativa');
+telaAtiva.classList.remove('ativa');
+
+const novaTela = document.getElementById(IdnovaTela);
+novaTela.classList.add('ativa');
+
+ }*/
